1b1626ab45b09b047e95d19b5d5386fc2540048f
Vignesh-Cherisher <vignesh.muruganandan@solitontech.com>
2023-08-03 11:18:16 +0530

    Function Name changed

--- Fibo.c
+++ Fibo.c
@@ -1,22 +1,38 @@
 #include<stdio.h>
 #include<stdio.h>  
-int SumSeries(int Arr[],int num);
+int n1=0,n2=1,n3,i,NumberOfElements,Sum; 
+void generateFibonacciSeries(int Arr[],int num){
+    if(num==0){
+        return;
+    }
+    if(num==1){
+        Arr[0]=0;
+    }
+    
+    
+    Arr[0]=n1;
+    Arr[1]=n2;
+    if(num==2){
+        return;
+    }
+    for(i=2;i<NumberOfElements;++i)
+    {    
+        n3=n1+n2;     
+        Arr[i]=n3;
+        n1=n2;    
+        n2=n3;    
+    }
+    return;
+}
 int main()    
 {    
-    int n1=0,n2=1,n3,i,NumberOfElements,Sum;    
     printf("Enter the number of elements : ");    
     scanf("%d",&NumberOfElements);    
     int arr[NumberOfElements];
-    printf("\nSeries : %d %d",n1,n2);
-    arr[0]=n1;
-    arr[1]=n2;
-    for(i=2;i<NumberOfElements;++i)
-    {    
-        n3=n1+n2;    
-        printf(" %d",n3);   
-        arr[i]=n3;
-        n1=n2;    
-        n2=n3;    
-    }  
+    generateFibonacciSeries(arr,NumberOfElements); 
+    printf("The Series : ");
+    for(int i=0;i<NumberOfElements;i++){
+        printf("%d ",arr[i]);
+    }
     return 0;  
 }  
